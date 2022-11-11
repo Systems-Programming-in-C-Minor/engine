b7da93aba801b0b26df36ac63ba2264e1cfeb0df
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-11-11 13:41:25 +0100

    Added base collider
    Fields and methods don't do anything for now.

---
+++ include/components/Collider.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Laerositus on 11/11/2022.
+//
+#pragma once
+#include "GameObject.hpp"
+
+#include <vector>
+
+class IRenderer;
+
+class Collider : public Component
+{
+    Collider();
+    ~Collider();
+
+    bool m_ignore_collision_physics;
+
+public:
+    std::vector<GameObject> collision_since_previous_tick();
+//    void render(IRenderer renderer);
+
+    bool get_ignore_collision_physics();
+    void set_ignore_collision_physics(bool icp);
+};
---
+++ src/components/Collider.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Laerositus on 11/11/2022.
+//
+//#include
+#include "components/Collider.hpp"
+
+std::vector<GameObject> Collider::collision_since_previous_tick(){
+    std::vector<GameObject> collisions;
+
+    return collisions;
+}
+
+//void Collider::render(IRenderer renderer){
+//
+//}
+
+bool Collider::get_ignore_collision_physics() {
+    return m_ignore_collision_physics;
+}
+
+void Collider::set_ignore_collision_physics(bool icp) {
+    m_ignore_collision_physics = icp;
+}
