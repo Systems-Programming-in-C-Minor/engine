5655aa7a125cd2905eb6f63f0dd587000a33af72
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-13 17:18:06 +0100

    Add sprite class

---
+++ src/components/Sprite.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lars on 13-11-22.
+//
+
+#include "../include/components/Sprite.hpp"
+
+Sprite::Sprite(std::string sprite, Color color, bool flipX, bool flipY, int sortingLayer, int orderInLayer) :
+        sprite(std::move(sprite)),
+        color(color),
+        flipX(flipX),
+        flipY(flipY),
+        sortingLayer(sortingLayer),
+        orderInLayer(orderInLayer) {}
+
+void Sprite::render(IRenderer renderer, bool isWorldSpace) const {}
