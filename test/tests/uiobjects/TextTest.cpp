c86894d2a7ffa308f341c3be1ad235f93848af17
Marco Tramper <marco1088@hotmail.nl>
2022-11-13 20:23:52 +0100

    fix test

--- test/tests/uiobjects/TextTest.cpp
+++ test/tests/uiobjects/TextTest.cpp
@@ -6,20 +6,21 @@ TEST(TextTest, TextIsInitializedCorrectly)
     Text testObject("text", "font", 10, Alignment::center);
     ASSERT_EQ(testObject.getText(), "text");
     ASSERT_EQ(testObject.getFont(), "font");
-    ASSERT_EQ(testObject.getSize(), 1);
+    ASSERT_EQ(testObject.getSize(), 10);
     ASSERT_EQ(testObject.getAlignment(), Alignment::center);
 }
 
 TEST(TextTest, TextIsSetCorrectly)
 {
     Text testObject("text", "font", 10, Alignment::center);
+    
     testObject.setText("new text");
     testObject.setFont("new font");
-    testObject.setSize(2);
+    testObject.setSize(20);
     testObject.setAlignment(Alignment::left);
 
     ASSERT_EQ(testObject.getText(), "new text");
     ASSERT_EQ(testObject.getFont(), "new font");
-    ASSERT_EQ(testObject.getSize(), 2);
+    ASSERT_EQ(testObject.getSize(), 20);
     ASSERT_EQ(testObject.getAlignment(), Alignment::left);
 }
