b25ed4cd2de9b8d8bc0c7d6573397ae88a730372
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-13 17:17:40 +0100

    Add render interfaces

---
+++ include/renderer/IRenderable.hpp
@@ -0,0 +1,12 @@
+#ifndef ENGINE_IRENDERABLE_HPP
+#define ENGINE_IRENDERABLE_HPP
+
+#include "IRenderer.hpp"
+
+class IRenderable {
+
+    virtual void render(IRenderer renderer, bool isWorldSpace = true) const = 0;
+
+};
+
+#endif //ENGINE_IRENDERABLE_HPP
---
+++ include/renderer/IRenderer.hpp
@@ -0,0 +1,12 @@
+//
+// Created by lars on 13-11-22.
+//
+
+#ifndef ENGINE_IRENDERER_HPP
+#define ENGINE_IRENDERER_HPP
+
+class IRenderer {
+    // TODO implement further
+};
+
+#endif //ENGINE_IRENDERER_HPP
