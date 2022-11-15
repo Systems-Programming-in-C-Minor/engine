e09af453344512357e3da0596903f375a09a3620
marco_rivia <marco1088@hotmail.nl>
2022-11-15 15:48:15 +0100

    remove cpp file, expand header with collider methods

--- include/behaviourscript.hpp
+++ include/behaviourscript.hpp
@@ -14,4 +14,22 @@ public:
      * @brief Continuous trigger
      */
     virtual void on_update();
+
+//    /**
+//     * @brief Sent when another object enters a trigger collider
+//     *        attached to this object (2D physics only).
+//     */
+//    virtual void OnTriggerEnter2D(const Collider& collider);
+//
+//    /**
+//     * @brief Sent when another object leaves a trigger collider
+//     *        attached to this object (2D physics only).
+//     */
+//    virtual void OnTriggerExit2D(const Collider& collider);
+//
+//    /**
+//     * @brief Sent each frame where another object is within a trigger
+//     *        collider attached to this object (2D physics only).
+//     */
+//    virtual void OnTriggerStay2D(const Collider& collider);
 };
