9e98927b214a04ef2e308ff7b858778a3100b303
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-11-15 18:43:22 +0100

    Updated Rigidbody class

---
+++ include/components/Rigidbody.hpp
@@ -0,0 +1,42 @@
+#ifndef ENGINE_RIGIDBODY_HPP
+#define ENGINE_RIGIDBODY_HPP
+
+#include "Component.hpp"
+//#include "Vector2d"  TODO include Vector2d class
+
+enum class BodyType
+{
+    static_body,
+    dynamic_body,
+    kinematic_body
+};
+
+class RigidBody : public Component
+{
+    RigidBody();
+    ~RigidBody();
+
+    double _mass;
+    double _gravity_scale;
+    BodyType _bodytype;
+
+public:
+//    void apply_force(Vector2d force); TODO uncomment code
+    void apply_torque(float torque);
+//    void set_linear_velocity(Vector2d velocity); TODO uncomment code
+//    Vector2d get_linear_velocity(); TODO uncomment code
+    void set_angular_velocity(float angle);
+    float get_angular_velocity();
+//    void apply_linear_impulse(Vector2d inpulse); TODO uncomment code
+//    void apply_angular_impulse(Vector2d impulse); TODO uncomment code
+
+    double get_mass();
+    void set_mass(double m);
+    double get_gravity_scale();
+    void set_gravity_scale(double gs);
+    BodyType get_bodytype();
+    void set_bodytype(BodyType bt);
+
+};
+
+#endif //ENGINE_RIGIDBODY_HPP
