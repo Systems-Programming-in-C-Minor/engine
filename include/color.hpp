8f4ecc511330bd6ea952f4aff6d4aedb16dab8df
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-15 11:26:53 +0100

    Add color implementation

--- include/color.hpp
+++ include/color.hpp
@@ -5,36 +5,10 @@ class Color {
 public:
     Color(double red, double green, double blue, double alpha);
 
-    static const Color &white() { return _white; }
-
-    static const Color &red() { return _red; }
-
-    static const Color &green() { return _green; }
-
-    static const Color &blue() { return _blue; }
-
-    static const Color &cyan() { return _cyan; }
-
-    static const Color &magenta() { return _magenta; }
-
-    static const Color &yellow() { return _yellow; }
-
-    static const Color &black() { return _black; }
-
-private:
-    double r;
-    double g;
-    double b;
-    double a;
-
-    static Color _white;
-    static Color _red;
-    static Color _green;
-    static Color _blue;
-    static Color _cyan;
-    static Color _magenta;
-    static Color _yellow;
-    static Color _black;
+    const double r;
+    const double g;
+    const double b;
+    const double a;
 };
 
 #endif //ENGINE_COLOR_HPP
