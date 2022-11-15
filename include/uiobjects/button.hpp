954906864f0376453462019c3bf97009ea56bb94
marco_rivia <marco1088@hotmail.nl>
2022-11-15 10:06:42 +0100

    add uiobjects

---
+++ include/uiobjects/button.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <functional>
+#include "uiobject.hpp"
+
+class Button : public UIObject {
+public:
+    /**
+     * @brief This function is called when the button is clicked, which
+     *        will trigger a call to the registered onClick member.
+     */
+    void click();
+
+    /**
+     * @brief Register the onClick handler to be used when the button is clicked.
+     * @param callback The function to register, usually a lambda. But this can be
+     *        any kind of callable.
+     */
+    void on_click(std::function<void()> callback) { _on_click = callback; }
+
+    void set_interactable(bool is_interactable);
+
+    [[nodiscard]] bool get_interactable() const;
+
+private:
+    /**
+     * @brief When false, the button will not react to clicks.
+     */
+    bool _interactable;
+
+    /**
+     * @brief The registered click handler.
+     */
+    std::function<void()> _on_click;
+};
