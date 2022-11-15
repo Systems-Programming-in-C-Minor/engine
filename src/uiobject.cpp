97c7586206386b271fa1060715643b5b00e1893e
marco_rivia <marco1088@hotmail.nl>
2022-11-15 09:42:51 +0100

    fix private variable names

--- src/uiobject.cpp
+++ src/uiobject.cpp
@@ -1,17 +1,17 @@
 #include "uiobject.hpp"
 
 double UIObject::get_height() const {
-    return height;
+    return _height;
 }
 
 double UIObject::get_width() const {
-    return width;
+    return _width;
 }
 
 void UIObject::set_height(double new_height) {
-    height = new_height;
+    _height = new_height;
 }
 
 void UIObject::set_width(double new_width) {
-    width = new_width;
+    _width = new_width;
 }
