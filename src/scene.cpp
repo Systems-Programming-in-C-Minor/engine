284f2592ebe0624cf4e7277cf1687d0c7336ba66
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-16 15:10:02 +0100

    Implemented tick and render calls

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,18 +1,18 @@
+#include "gameobject.hpp"
 #include "scene.hpp"
+
 
 void Scene::tick()
 {
 	for (auto& gameobject : gameobjects) {
-		// TODO Call tick on gameobject here. Missing tick func
-		// gameobject.tick();
+		gameobject->tick();
 	}
 }
 
 void Scene::render(IRenderer& renderer)
 {
 	for (auto& gameobject : gameobjects) {
-		// TODO Render gameobjects here. Missing render func and renderer
-		// gameobject.render(renderer);
+		gameobject->render(renderer);
 	}
 }
 
