b5076c39e31e084a0a06bd1aa66fe7ee76a73c20
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-16 13:54:54 +0100

    Made Component.is_active() virtual for overriding

--- include/components/component.hpp
+++ include/components/component.hpp
@@ -6,7 +6,7 @@ protected:
     Component() = default;
     bool active = true;
 public:
-    void set_active(bool is_active);
+    virtual void set_active(bool is_active);
     [[nodiscard]] bool get_active() const;
 
     virtual ~Component() = default;
