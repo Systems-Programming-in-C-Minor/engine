33a7ecb8afc84b1cc8ad1303d9665d1bf499310a
marco_rivia <marco1088@hotmail.nl>
2022-11-17 10:05:23 +0100

    fix naming convention

--- include/behaviourscript.hpp
+++ include/behaviourscript.hpp
@@ -6,31 +6,28 @@ public:
     /**
      * @brief Function gets called on initialization
      */
-    virtual void OnStart();
+    virtual void on_start();
 
     /**
      * @brief Function gets called continuously
      */
-    virtual void OnUpdate();
+    virtual void on_update();
 
-//        /**
-//         * @brief Sent when another object enters a trigger collider
-//         *        attached to this object (2D physics only).
-//         * @spicapi
-//         */
-//        virtual void OnTriggerEnter2D(const Collider& collider);
+//    /**
+//     * @brief Sent when another object enters a trigger collider
+//     *        attached to this object (2D physics only).
+//     */
+//    virtual void on_trigger_enter_2D(const Collider& collider);
 //
-//        /**
-//         * @brief Sent when another object leaves a trigger collider
-//         *        attached to this object (2D physics only).
-//         * @spicapi
-//         */
-//        virtual void OnTriggerExit2D(const Collider& collider);
+//    /**
+//     * @brief Sent when another object leaves a trigger collider
+//     *        attached to this object (2D physics only).
+//     */
+//    virtual void on_trigger_exit_2D(const Collider& collider);
 //
-//        /**
-//         * @brief Sent each frame where another object is within a trigger
-//         *        collider attached to this object (2D physics only).
-//         * @spicapi
-//         */
-//        virtual void OnTriggerStay2D(const Collider& collider);
+//    /**
+//     * @brief Sent each frame where another object is within a trigger
+//     *        collider attached to this object (2D physics only).
+//     */
+//    virtual void on_trigger_stay_2D(const Collider& collider);
 };
