6206163d0841988fb5e4aa1d57095c368a921e03
Maik de Kruif <maik@maik.dev>
2022-11-17 11:42:25 +0100

    Actually test the correct object and test setters

--- test/tests/uiobject_tests.cpp
+++ test/tests/uiobject_tests.cpp
@@ -3,12 +3,18 @@
 
 #define test_ui_object UIObject("name", "tag", 10, 10)
 
-class TestUIObject : public UIObject {
-};
-
-TEST(UIObjectTest, TestGetProperties)
+TEST(UIObjectTest, TestGetWidthHeight)
 {
     auto ui_object = test_ui_object;
-    ASSERT_EQ(10, test_ui_object.get_width());
-    ASSERT_EQ(10, test_ui_object.get_height());
+    ASSERT_EQ(10, ui_object.get_width());
+    ASSERT_EQ(10, ui_object.get_height());
+}
+
+TEST(UIObjectTest, TestSetWidthHeight)
+{
+    auto ui_object = test_ui_object;
+    ui_object.set_width(20);
+    ui_object.set_height(30);
+    ASSERT_EQ(20, ui_object.get_width());
+    ASSERT_EQ(30, ui_object.get_height());
 }
