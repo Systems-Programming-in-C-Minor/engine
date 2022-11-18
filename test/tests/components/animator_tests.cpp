0996479a95ae15f7761bb12c17ec8b45d28606bc
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-18 13:00:52 +0100

    Add animator not active test

--- test/tests/components/animator_tests.cpp
+++ test/tests/components/animator_tests.cpp
@@ -159,6 +159,32 @@ TEST(AnimatorTest, AnimateWithoutPlay) {
 
     for (int i = 0; i < 5; i++) {
         game_object.tick();
-        EXPECT_NE(game_object.get_component<Sprite>(), sprite_2);
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+    }
+}
+
+
+TEST(AnimatorTest, AnimateNotActive) {
+    auto game_object = test_game_object;
+
+    const auto sprite_1 = std::make_shared<Sprite>("/path/to", Color(1, 0, 0, 0), false, false, 1, 1);
+    const auto sprite_2 = std::make_shared<Sprite>("/path/to", Color(2, 0, 0, 0), false, false, 1, 1);
+
+    const Animator::Sprites sprites{sprite_1, sprite_2};
+    const auto animator = std::make_shared<Animator>(sprites, 20);
+
+    game_object.add_component(animator);
+    game_object.add_component(sprite_1);
+
+    animator->play();
+
+    animator->set_active(false);
+
+    // Check sprite 1
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+
+    for (int i = 0; i < 5; i++) {
+        game_object.tick();
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
     }
 }
