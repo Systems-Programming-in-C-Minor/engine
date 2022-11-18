7cc243c015e5c8c4551be4b79dbd0e6cf32ea43d
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-19 00:22:02 +0100

    Add box2d world to scene

--- include/scene.hpp
+++ include/scene.hpp
@@ -3,28 +3,32 @@
 
 #include <vector>
 #include <memory>
+#include "box2d/box2d.h"
 
 class IRenderer;
+
 class GameObject;
 
-class Scene
-{
+class Scene {
 public:
-	std::vector<std::shared_ptr<GameObject>> gameobjects;
-	
-	/**
-	 * Simulates a tick on this scene's gameobjects
-	 */
-	void tick() const;
+    std::vector<std::shared_ptr<GameObject>> gameobjects;
 
-	/**
-	 * Calls the render functions on this scene's gameobjects
-	 * 
-	 * @param[in] renderer Reference to the active renderer
-	 */
-	void render(IRenderer& renderer) const;
+    /**
+     * Simulates a tick on this scene's gameobjects
+     */
+    void tick() const;
 
-	Scene();
+    /**
+     * Calls the render functions on this scene's gameobjects
+     *
+     * @param[in] renderer Reference to the active renderer
+     */
+    void render(IRenderer &renderer) const;
+
+    Scene();
+
+private:
+    const std::unique_ptr<b2World> _world = std::make_unique<b2World>(b2Vec2(0.0f, 0.0f));
 };
 
 #endif // ENGINE_SCENE_H_Kh3IZygul2
