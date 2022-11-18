1b69c783d935f0bdf09ff015add550cae64d0155
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-18 12:46:35 +0100

    Add animator play and stop test

--- test/tests/components/animator_tests.cpp
+++ test/tests/components/animator_tests.cpp
@@ -79,6 +79,37 @@ TEST(AnimatorTest, AnimateWithLoop) {
     EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
 }
 
+TEST(AnimatorTest, AnimateAndStop) {
+    auto game_object = test_game_object;
+
+    const auto sprite_1 = std::make_shared<Sprite>("/path/to", Color(1, 0, 0, 0), false, false, 1, 1);
+    const auto sprite_2 = std::make_shared<Sprite>("/path/to", Color(2, 0, 0, 0), false, false, 1, 1);
+    const auto sprite_3 = std::make_shared<Sprite>("/path/to", Color(3, 0, 0, 0), false, false, 1, 1);
+
+    const Animator::Sprites sprites{sprite_1, sprite_2, sprite_3};
+    const auto animator = std::make_shared<Animator>(sprites, 20);
+
+    game_object.add_component(animator);
+    game_object.add_component(sprite_1);
+
+    animator->play(true);
+
+    // Check sprite 1
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+
+    // Tick and check sprite 2
+    game_object.tick();
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_2);
+
+    // Stop
+    animator->stop();
+
+    for (int i = 0; i < 5; i++) {
+        game_object.tick();
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_2);
+    }
+}
+
 TEST(AnimatorTest, AnimateWithoutPlay) {
     auto game_object = test_game_object;
 
@@ -94,7 +125,7 @@ TEST(AnimatorTest, AnimateWithoutPlay) {
     // Check sprite 1
     EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
 
-    for (int i = 0; i < 40; i++) {
+    for (int i = 0; i < 5; i++) {
         game_object.tick();
         EXPECT_NE(game_object.get_component<Sprite>(), sprite_2);
     }
