c42c607e5343432723aa94e97b863526872401e8
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-18 12:44:43 +0100

    Add animator loop test

--- test/tests/components/animator_tests.cpp
+++ test/tests/components/animator_tests.cpp
@@ -17,7 +17,8 @@ TEST(AnimatorTest, Animate) {
 
     game_object.add_component(animator);
     game_object.add_component(sprite_1);
-    game_object.get_component<Animator>()->play();
+
+    animator->play();
 
     // Check sprite 1
     EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
@@ -29,6 +30,50 @@ TEST(AnimatorTest, Animate) {
     // Last frame
     game_object.tick();
     EXPECT_EQ(game_object.get_component<Sprite>(), sprite_3);
+
+    game_object.tick();
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+}
+
+TEST(AnimatorTest, AnimateWithLoop) {
+    auto game_object = test_game_object;
+
+    const auto sprite_1 = std::make_shared<Sprite>("/path/to", Color(1, 0, 0, 0), false, false, 1, 1);
+    const auto sprite_2 = std::make_shared<Sprite>("/path/to", Color(2, 0, 0, 0), false, false, 1, 1);
+    const auto sprite_3 = std::make_shared<Sprite>("/path/to", Color(3, 0, 0, 0), false, false, 1, 1);
+
+    const Animator::Sprites sprites{sprite_1, sprite_2, sprite_3};
+    const auto animator = std::make_shared<Animator>(sprites, 20);
+
+    game_object.add_component(animator);
+    game_object.add_component(sprite_1);
+
+    animator->play(true);
+
+    // Check sprite 1
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+
+    // Tick and check sprite 2
+    game_object.tick();
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_2);
+
+    // Last frame
+    game_object.tick();
+    EXPECT_EQ(game_object.get_component<Sprite>(), sprite_3);
+
+    for (int i = 0; i < 5; ++i) {
+        // Check sprite 1
+        game_object.tick();
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
+
+        // Tick and check sprite 2
+        game_object.tick();
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_2);
+
+        // Last frame
+        game_object.tick();
+        EXPECT_EQ(game_object.get_component<Sprite>(), sprite_3);
+    }
 
     game_object.tick();
     EXPECT_EQ(game_object.get_component<Sprite>(), sprite_1);
