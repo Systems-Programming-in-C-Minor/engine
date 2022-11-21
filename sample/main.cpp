840a641503bf519d66a145184a9df07a41421ab5
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-21 04:18:58 +0100

    Added functional functions into sample

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,6 +1,9 @@
 #include "sdlrenderer.hpp"
+#include "color.hpp"
 
 int main(int argc, char* argv[])
 {
-	SdlRenderer renderer = SdlRenderer(800, 600);
+	const auto renderer = SdlRenderer(800, 600);
+	renderer.clear(Color(0.0, 0.0, 0.0, 255.0));
+	renderer.push_to_screen();
 }
