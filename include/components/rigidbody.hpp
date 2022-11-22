0d8a65ed1fc76c70e8e808d6d207941c841d44c7
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-22 16:44:26 +0100

    Implement b2BodyDef and b2Body into rigidbody

--- include/components/rigidbody.hpp
+++ include/components/rigidbody.hpp
@@ -1,38 +1,55 @@
 #pragma once
+
 #include "component.hpp"
 #include "vector2d.hpp"
+#include "memory"
 
-enum class BodyType
-{
-    static_body,
-    dynamic_body,
-    kinematic_body
+enum class BodyType {
+    static_body = 0,
+    kinematic_body,
+    dynamic_body
 };
 
-class RigidBody : public Component
-{
-    RigidBody();
-    ~RigidBody();
+class b2Body;
 
-    double _mass;
-    double _gravity_scale;
-    BodyType _bodytype;
+class Scene;
 
+class RigidBody : public Component {
 public:
+
+    /**
+    * @param body
+    */
+    RigidBody(Scene &scene, BodyType type, Vector2d vector, double gravity_scale);
+
     void apply_force(Vector2d force);
+
     void apply_torque(float torque);
+
     void set_linear_velocity(Vector2d velocity);
+
     Vector2d get_linear_velocity();
+
     void set_angular_velocity(float angle);
+
     float get_angular_velocity();
+
     void apply_linear_impulse(Vector2d inpulse);
+
     void apply_angular_impulse(Vector2d impulse);
 
     double get_mass();
+
     void set_mass(double m);
+
     double get_gravity_scale();
+
     void set_gravity_scale(double gs);
+
     BodyType get_bodytype();
+
     void set_bodytype(BodyType bt);
 
+private:
+    b2Body *_body;
 };
