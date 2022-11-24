89ba15925e23916e17b8f45eeab1ae21ff5618d6
marco_rivia <marco1088@hotmail.nl>
2022-11-24 08:34:02 +0100

    fix tests

--- src/debug.cpp
+++ src/debug.cpp
@@ -4,17 +4,17 @@
 
 void Debug::log(const std::string &message)
 {
-    std::cout << "INFO - " << &message << std::endl;
+    std::cout << "INFO - " << message << std::endl;
 }
 
 void Debug::log_warning(const std::string &warning)
 {
-    std::cerr << "WARNING - " << &warning << std::endl;
+    std::cerr << "WARNING - " << warning << std::endl;
 }
 
 void Debug::log_error(const std::string &error)
 {
-    std::cerr << "ERROR - " << &error << std::endl;
+    std::cerr << "ERROR - " << error << std::endl;
 }
 
 void Debug::draw_line(const Vector2d &start, const Vector2d &end, const Color &color)
