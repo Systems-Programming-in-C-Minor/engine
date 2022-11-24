6ba30e0b3fb3635af625a66c780aee2ce670990a
marco_rivia <marco1088@hotmail.nl>
2022-11-24 09:24:03 +0100

    convert color to SDL_Color

--- include/color.hpp
+++ include/color.hpp
@@ -1,15 +1,21 @@
 #ifndef ENGINE_COLOR_HPP
 #define ENGINE_COLOR_HPP
 
+#include <cstdint>
+
+class SDL_Color;
+
 class Color {
 public:
-    Color(int r = 0, int g = 0, int b = 0, int a = 0);
+    Color(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 0);
     ~Color() = default;
 
-    int _r;
-    int _g;
-    int _b;
-    int _a;
+    uint8_t _r;
+    uint8_t _g;
+    uint8_t _b;
+    uint8_t _a;
+
+    [[nodiscard]] SDL_Color get_color() const;
 
 [[nodiscard]] bool equals(const Color &other) const;
 [[nodiscard]] bool operator==(const Color &other) const;
--- src/color.cpp
+++ src/color.cpp
@@ -1,6 +1,7 @@
 #include "color.hpp"
+#include "SDL2pp/Color.hh"
 
-Color::Color(int r, int g, int b, int a): _r(r), _g(g), _b(b), _a(a){}
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a): _r(r), _g(g), _b(b), _a(a){}
 
 bool Color::equals(const Color& other) const {
     return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
@@ -8,4 +9,8 @@ bool Color::equals(const Color& other) const {
 
 bool Color::operator==(const Color& other) const {
     return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
+}
+
+SDL_Color Color::get_color() const {
+    return SDL_Color {_r, _g, _b, _a};
 }
