56f8ff2205907837cadaf1cf13ee4387dc010523
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-11-29 12:08:30 +0100

    Add event manager

---
+++ include/managers/event_manager.hpp
@@ -0,0 +1,20 @@
+
+#ifndef ENGINE_EVENT_MANAGER_HPP
+#define ENGINE_EVENT_MANAGER_HPP
+
+#include "memory"
+#include "map"
+#include "../interfaces/ievent.hpp"
+#include "vector"
+
+class EventManager {
+public:
+    void register_listener(std::string, void(*)());
+
+    void notify(IEvent event);
+
+private:
+    std::map<std::string, std::vector<void(*)()>> listeners;
+};
+
+#endif //ENGINE_EVENT_MANAGER_HPP
---
+++ src/managers/event_manager.cpp
@@ -0,0 +1,3 @@
+#include "../../include/managers/event_manager.hpp"
+
+void EventManager::register_listener(std::string, void (*)()) {}
