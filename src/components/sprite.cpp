0f83fdcd7cdb8c43352795625ddb4036bc2ac8c6
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-30 16:37:38 +0100

    Added texture loading

--- src/components/sprite.cpp
+++ src/components/sprite.cpp
@@ -1,13 +1,29 @@
+#include "gameobject.hpp"
+
 #include "components/sprite.hpp"
 
+#include "global.hpp"
+#include "sdltexture.hpp"
+#include "sdlrenderer.hpp"
+
+
 Sprite::Sprite(std::string sprite, Color color, bool flip_x, bool flip_y, int sorting_layer, int order_in_layer) :
-        sprite(std::move(sprite)),
+        _sprite(std::move(sprite)),
         color(color),
         flip_x(flip_x),
         flip_y(flip_y),
         sorting_layer(sorting_layer),
-        order_in_layer(order_in_layer) {}
+        order_in_layer(order_in_layer)
+{
+    load_texture();
+}
 
-void Sprite::render(IRenderer &renderer, bool is_world_space) const {
-    renderer.render_sprite(*this, *texture, is_world_space);
+void Sprite::render(bool is_world_space) const {
+    const auto renderer = Global::get_instance()->get_engine().get_renderer();
+    renderer->render_sprite(*this, *_texture, game_object->transform, is_world_space);
+}
+
+void Sprite::load_texture()
+{
+    _texture = std::make_shared<SdlTexture>(_sprite);
 }
