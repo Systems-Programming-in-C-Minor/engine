534bd733e67583516cb42eaacba615c59ef222a6
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-30 17:11:44 +0100

    Updated tests

--- test/mocks/mock_engine.hpp
+++ test/mocks/mock_engine.hpp
@@ -3,11 +3,14 @@
 
 #include "gmock/gmock.h"
 #include "engine.hpp"
+#include "mock_renderer.hpp"
 
 class MockEngine : public Engine {
 public:
+    MockEngine() : Engine(std::make_shared<MockRenderer>()) {};
     MOCK_METHOD(const Scene &, get_active_scene, (), (override, const));
     MOCK_METHOD(Scene &, get_active_scene, (), (override));
+    MOCK_METHOD(std::shared_ptr<IRenderer>, get_renderer, (), (override, const));
 };
 
 #endif // ENGINE_MOCK_ENGINE_HPP
--- test/mocks/mock_gameobject.hpp
+++ test/mocks/mock_gameobject.hpp
@@ -10,7 +10,7 @@ public:
 	MockGameObject(const std::string& name, const std::string& tag, bool is_world_space = true) : GameObject(name, tag, is_world_space) {}
 
 	MOCK_METHOD(void, tick, (), (override));
-	MOCK_METHOD(void, render, (IRenderer&), (override, const));
+	MOCK_METHOD(void, render, (), (override, const));
 };
 
 #endif // ENGINE_MOCK_GAMEOBJECT_H_4LY8uw2Eo3
