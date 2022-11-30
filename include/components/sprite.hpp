c261bd1f9a0eaae6775fa099435fd57d1da82c09
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-30 16:36:46 +0100

    Removed renderer from params of render functions

--- include/components/sprite.hpp
+++ include/components/sprite.hpp
@@ -12,8 +12,8 @@ class Sprite : public Component, public IRenderable {
 public:
     Sprite(std::string sprite, Color color, bool flip_x, bool flip_y, int sorting_layer, int order_in_layer);
 
-    void render(IRenderer &renderer, bool is_world_space) const override;
 
+    void render(bool is_world_space) const override;
 private:
     std::string sprite;
     Color color;
