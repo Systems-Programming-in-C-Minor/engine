cf6bedbeffabd615a452062f9d5b15be1a57fdbf
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-30 17:04:05 +0100

    Added te ability to get the parent gameobject of a component

--- include/components/component.hpp
+++ include/components/component.hpp
@@ -1,11 +1,15 @@
 #ifndef ENGINE_COMPONENT_HPP
 #define ENGINE_COMPONENT_HPP
 
+class GameObject;
+
 class Component {
 protected:
     Component() = default;
     bool active = true;
+    friend class GameObject;
 public:
+    GameObject* game_object = nullptr;
     virtual void set_active(bool is_active);
     [[nodiscard]] bool get_active() const;
 
