918e77a18433e3fb33428d42213dbf005be9ca57
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-05 00:09:32 +0100

    Converted transform to use floats everywhere instead of doubles

--- include/transform.hpp
+++ include/transform.hpp
@@ -8,19 +8,19 @@
 class Transform
 {
 public:
-	[[nodiscard]] double get_angle();
-	void set_angle(double angle = 0.0);
-	[[nodiscard]] double get_scale();
-	void set_scale(double scale = 1.0);
-	[[nodiscard]] Vector2d get_position();
-	void set_position(Vector2d& position);
+	[[nodiscard]] float get_angle() const;
+	void set_angle(float angle = 0.f);
+	[[nodiscard]] float get_scale() const;
+	void set_scale(float scale = 1.f);
+	[[nodiscard]] Vector2d get_position() const;
+	void set_position(const Vector2d& position);
 
-	Transform(const Vector2d& position, double angle = 0.0, double scale = 1.0);
+	Transform(const Vector2d& position, float angle = 0.f, float scale = 1.f);
 
 private:
 	Vector2d _position;
-	double _angle;
-	double _scale;
+	float _angle;
+	float _scale;
 };
 
 #endif // ENGINE_TRANSFORM_H_sZYCcKEA4x
--- src/transform.cpp
+++ src/transform.cpp
@@ -1,37 +1,37 @@
 #include "transform.hpp"
 
 
-double Transform::get_angle()
+float Transform::get_angle() const
 {
 	return _angle;
 }
 
-void Transform::set_angle(double angle)
+void Transform::set_angle(float angle)
 {
 	_angle = angle;
 }
 
-double Transform::get_scale()
+float Transform::get_scale() const
 {
 	return _scale;
 }
 
-void Transform::set_scale(double scale)
+void Transform::set_scale(float scale)
 {
 	_scale = scale;
 }
 
-Vector2d Transform::get_position()
+Vector2d Transform::get_position() const
 {
 	return _position;
 }
 
-void Transform::set_position(Vector2d & position)
+void Transform::set_position(const Vector2d& position)
 {
 	_position = position;
 }
 
-Transform::Transform(const Vector2d& position, double angle, double scale):
+Transform::Transform(const Vector2d& position, float angle, float scale):
 	_position(position),
 	_angle(angle),
 	_scale(scale) {}
