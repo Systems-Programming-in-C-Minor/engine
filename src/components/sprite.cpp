bb25db47ef83da98e2c761bc4dff5c86a8300103
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-04 16:34:56 +0100

    Added ability to retrieve world size and resolution of sprite

--- src/components/sprite.cpp
+++ src/components/sprite.cpp
@@ -7,15 +7,16 @@
 #include "sdlrenderer.hpp"
 
 
-Sprite::Sprite(std::string sprite, Color color, bool flip_x, bool flip_y, int sorting_layer, int order_in_layer) :
+Sprite::Sprite(std::string sprite, Color color, bool flip_x, bool flip_y, int sorting_layer, int order_in_layer, int pixels_to_meters) :
+		res_x(0),
+		res_y(0),
 		_sprite(std::move(sprite)),
 		color(color),
 		flip_x(flip_x),
 		flip_y(flip_y),
 		sorting_layer(sorting_layer),
 		order_in_layer(order_in_layer),
-		_res_x(0),
-		_res_y(0)
+		pixels_to_meters(pixels_to_meters)
 {
 	load_texture();
 }
@@ -25,7 +26,19 @@ void Sprite::render(bool is_world_space) const {
 	renderer->render_sprite(*this, *_texture, game_object->transform, is_world_space);
 }
 
+unsigned int Sprite::get_size_x() const
+{
+	return res_x / pixels_to_meters;
+}
+
+unsigned int Sprite::get_size_y() const
+{
+	return res_y / pixels_to_meters;
+}
+
+
+
 void Sprite::load_texture()
 {
-	_texture = std::make_shared<SdlTexture>(_sprite, &_res_x, &_res_y);
+	_texture = std::make_shared<SdlTexture>(_sprite, &res_x, &res_y);
 }
