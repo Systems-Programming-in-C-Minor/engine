cf9fa15aeb03acd15bfb54fcd6932ae09d720fbf
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-05 12:16:02 +0100

    Indentation consistenct

--- include/color.hpp
+++ include/color.hpp
@@ -5,7 +5,7 @@ class SdlRenderer;
 
 namespace SDL2pp
 {
-    class Color;
+	class Color;
 }
 
 class Color {
@@ -14,16 +14,16 @@ private:
 	explicit Color(const SDL2pp::Color& color);
 	explicit operator SDL2pp::Color() const;
 public:
-    Color(unsigned int r = 0, unsigned int g = 0, unsigned int b = 0, unsigned int a = 0);
-    ~Color() = default;
+	Color(unsigned int r = 0, unsigned int g = 0, unsigned int b = 0, unsigned int a = 0);
+	~Color() = default;
 
-    unsigned int _r;
-    unsigned int _g;
-    unsigned int _b;
-    unsigned int _a;
+	unsigned int _r;
+	unsigned int _g;
+	unsigned int _b;
+	unsigned int _a;
 
-[[nodiscard]] bool equals(const Color &other) const;
-[[nodiscard]] bool operator==(const Color &other) const;
+	[[nodiscard]] bool equals(const Color &other) const;
+	[[nodiscard]] bool operator==(const Color &other) const;
 };
 
 #endif //ENGINE_COLOR_HPP
--- src/color.cpp
+++ src/color.cpp
@@ -6,18 +6,18 @@ Color::Color(unsigned int r, unsigned int g, unsigned int b, unsigned int a): _r
 Color::Color(const SDL2pp::Color& color): _r(color.r), _g(color.g), _b(color.b), _a(color.a){}
 
 bool Color::equals(const Color& other) const {
-    return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
+	return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
 }
 
 bool Color::operator==(const Color& other) const {
-    return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
+	return _r == other._r && _g == other._g && _b == other._b && _a == other._a;
 }
 
 Color::operator SDL2pp::Color() const {
-    return SDL2pp::Color {
-        static_cast<Uint8>(_r),
-        static_cast<Uint8>(_g),
-        static_cast<Uint8>(_b),
-        static_cast<Uint8>(_a)
-    };
+	return SDL2pp::Color {
+		static_cast<Uint8>(_r),
+		static_cast<Uint8>(_g),
+		static_cast<Uint8>(_b),
+		static_cast<Uint8>(_a)
+	};
 }
