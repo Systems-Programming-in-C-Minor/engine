b6010c2b9c9b4377ba94ab489c33f81b04dfa529
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-05 12:15:50 +0100

    Color conversion operators made private

--- include/color.hpp
+++ include/color.hpp
@@ -1,15 +1,20 @@
 #ifndef ENGINE_COLOR_HPP
 #define ENGINE_COLOR_HPP
 
+class SdlRenderer;
+
 namespace SDL2pp
 {
     class Color;
 }
 
 class Color {
+private:
+	friend class SdlRenderer;
+	explicit Color(const SDL2pp::Color& color);
+	explicit operator SDL2pp::Color() const;
 public:
     Color(unsigned int r = 0, unsigned int g = 0, unsigned int b = 0, unsigned int a = 0);
-    explicit Color(const SDL2pp::Color& color);
     ~Color() = default;
 
     unsigned int _r;
@@ -19,7 +24,6 @@ public:
 
 [[nodiscard]] bool equals(const Color &other) const;
 [[nodiscard]] bool operator==(const Color &other) const;
-explicit operator SDL2pp::Color() const;
 };
 
 #endif //ENGINE_COLOR_HPP
