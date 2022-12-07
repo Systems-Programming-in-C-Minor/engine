4fdb24ed0cd959fa015c47261a2635f8e71031d2
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-07 15:36:36 +0100

    Added link between PolygonCollider and Box2D

--- include/components/colliders/polygoncollider.hpp
+++ include/components/colliders/polygoncollider.hpp
@@ -3,8 +3,11 @@
 
 #include "vector2d.hpp"
 #include "collider.hpp"
+#include "utils/collidernormalenum.hpp"
 
 #include <vector>
+
+class b2ChainShape;
 
 class Vector2d;
 
@@ -15,15 +18,17 @@ class Vector2d;
 class PolygonCollider : public Collider
 {
 public:
-    explicit PolygonCollider(std::vector<Vector2d> points);
-    explicit PolygonCollider(const std::string& path);
+    explicit PolygonCollider(std::vector<Vector2d> points, ColliderNormal collider_normal = ColliderNormal::outwards);
+    explicit PolygonCollider(const std::string& path, ColliderNormal collider_normal = ColliderNormal::outwards);
 
     [[nodiscard]] std::vector<Vector2d> get_points() const;
 
 private:
+    std::shared_ptr<b2ChainShape> _shape;
     std::vector<Vector2d> _points;
 
-    void construct_points(const std::string& path);
+    void get_points_from_file(const std::string& path);
+    void set_shape(ColliderNormal collider_normal);
 };
 
 #endif //COLLIDERS_POLYGONCOLLIDER_HPP
--- src/components/colliders/polygoncollider.cpp
+++ src/components/colliders/polygoncollider.cpp
@@ -1,21 +1,43 @@
 #include "components/colliders/polygoncollider.hpp"
+#include "box2d/b2_chain_shape.h"
 
-PolygonCollider::PolygonCollider(std::vector<Vector2d> points) : Collider(){
+PolygonCollider::PolygonCollider(std::vector<Vector2d> points, ColliderNormal collider_normal) : Collider(){
     _points = std::move(points);
+    set_shape(collider_normal);
 }
 
-PolygonCollider::PolygonCollider(const std::string& path): Collider() {
-    construct_points(path);
+PolygonCollider::PolygonCollider(const std::string& path, ColliderNormal collider_normal): Collider() {
+    get_points_from_file(path);
 }
 
 std::vector<Vector2d> PolygonCollider::get_points() const {
     return _points;
 }
 
-void PolygonCollider::construct_points(const std::string& path) {
+void PolygonCollider::get_points_from_file(const std::string& path) {
     /*
      * TODO Task: Implement SpriteCollider
      * https://app.clickup.com/t/377pt9y
      */
     _points = std::vector<Vector2d>{Vector2d{}};
+}
+
+void PolygonCollider::set_shape(ColliderNormal collider_normal) {
+    std::vector<b2Vec2> vectors;
+
+    if (collider_normal == ColliderNormal::outwards) {
+        for (auto i = _points.rbegin(); i != _points.rend(); ++i) {
+            vectors.push_back(static_cast<b2Vec2>(*i));
+        }
+    }
+
+    else if (collider_normal == ColliderNormal::inwards) {
+        for (auto& vector2d : _points) {
+            vectors.push_back(static_cast<b2Vec2>(vector2d));
+        }
+    }
+    
+    b2ChainShape chain;
+    chain.CreateLoop(vectors.data(), static_cast<int>(vectors.size()));
+    _shape = std::make_shared<b2ChainShape>(chain);
 }
