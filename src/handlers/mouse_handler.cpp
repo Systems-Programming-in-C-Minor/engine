fe11a924a35bd20b3f9cef15c7ece1900261ff5a
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-07 12:20:33 +0100

    Only fire pressed and release once

--- src/handlers/mouse_handler.cpp
+++ src/handlers/mouse_handler.cpp
@@ -24,17 +24,20 @@ void MouseHandler::tick() {
     int old_y = _y;
     auto input_pressed = fetch_input();
 
+    for (const auto pressed_key: input_pressed) {
+        if (std::find(_input_active.begin(), _input_active.end(), pressed_key) == _input_active.end())
+            continue;
+
+        auto pressed_event = MousePressedEvent(pressed_key);
+        Global::get_instance()->notify_event_manager(pressed_event);
+    }
+
     for (const auto active_key: _input_active) {
         if (std::find(input_pressed.begin(), input_pressed.end(), active_key) == input_pressed.end())
             continue;
 
         auto release_event = MouseReleasedEvent(active_key);
         Global::get_instance()->notify_event_manager(release_event);
-    }
-
-    for (const auto pressed_key: input_pressed) {
-        auto pressed_event = MousePressedEvent(pressed_key);
-        Global::get_instance()->notify_event_manager(pressed_event);
     }
 
     if (old_x != _x || old_y != _y) {
