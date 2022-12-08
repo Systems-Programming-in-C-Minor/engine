f2b3e8867022b9f4c8729183e8ea7c6b18403400
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-08 17:17:23 +0100

    Implemented pure virtual in CircleCollider

--- src/components/colliders/circlecollider.cpp
+++ src/components/colliders/circlecollider.cpp
@@ -5,3 +5,7 @@ CircleCollider::CircleCollider(double radius) : _radius(radius) {}
 double CircleCollider::get_radius() const {
     return _radius;
 }
+
+void CircleCollider::set_fixture(b2Body& body, float friction, float restitution) {
+
+}
