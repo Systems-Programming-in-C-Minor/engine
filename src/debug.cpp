89c32aba4e4f9a18939dd415ee41a195b6657f68
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-12-12 21:16:06 +0100

    Revert "Revert "Merge branch 'master' into Add-Input-class_CU-36pg7zn""
    This reverts commit a6ed71bbe9def716cd1ca78f9c00a6a8db489d39.

---
+++ src/debug.cpp
@@ -0,0 +1,23 @@
+#include "debug.hpp"
+
+#include <iostream>
+
+void Debug::log(const std::string &message)
+{
+    std::cout << "INFO - " << message << std::endl;
+}
+
+void Debug::log_warning(const std::string &warning)
+{
+    std::cerr << "WARNING - " << warning << std::endl;
+}
+
+void Debug::log_error(const std::string &error)
+{
+    std::cerr << "ERROR - " << error << std::endl;
+}
+
+void Debug::draw_line(const Vector2d &start, const Vector2d &end, const Color &color)
+{
+    //TODO: https://app.clickup.com/t/377qjj4
+}
