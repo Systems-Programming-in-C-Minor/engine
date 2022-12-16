2bf2c29e51be3974133472929f7ad8bca56b2156
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-16 12:26:22 +0100

    Added enum for different viewing spaces/dimensions

---
+++ include/enums/spaces.hpp
@@ -0,0 +1,12 @@
+#ifndef ENUMS_SPACES_HPP
+#define ENUMS_SPACES_HPP
+
+/**
+  * @brief Enumeration for different dimensions/spaces.
+  */
+enum class Space {
+    WORLD = 0,
+    SCREEN
+};
+
+#endif //ENUMS_SPACES_HPP  
