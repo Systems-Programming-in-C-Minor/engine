baf25895c6414dfdc3fb9b48e24b9154b45942c5
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-16 18:59:16 +0100

    Add random

---
+++ include/utils/random.hpp
@@ -0,0 +1,13 @@
+
+#ifndef ENGINE_RANDOM_HPP
+#define ENGINE_RANDOM_HPP
+
+class Random {
+public:
+    static int random(int max = 1, int min = 0);
+
+    static bool random_bool();
+};
+
+
+#endif //ENGINE_RANDOM_HPP
