c9b2589d1e9fc2661a4e5e06f7b3addf5ff68746
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-16 12:11:37 +0100

    Added anchor enum

---
+++ include/enums/anchor.hpp
@@ -0,0 +1,14 @@
+#ifndef ENUMS_ANCHOR_HPP
+#define ENUMS_ANCHOR_HPP
+
+/**
+  * @brief Enumeration for different anchor points.
+  */
+enum class Anchor {
+    LEFT_TOP = 0,
+    RIGHT_TOP,
+    RIGHT_BOT,
+    LEFT_BOT
+};
+
+#endif //ENUMS_ANCHOR_HPP
