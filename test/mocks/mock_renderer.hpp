276c963ae5651457dd4ac3810b09972ad214b8f2
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-19 22:46:44 +0100

    Fixed tests

--- test/mocks/mock_renderer.hpp
+++ test/mocks/mock_renderer.hpp
@@ -13,14 +13,14 @@ class MockRenderer : public IRenderer {
 public:
 	MOCK_METHOD(void, render_texture, (ITexture&, Transform&, float), (const, override));
 	MOCK_METHOD(void, render_texture, (ITexture&, Transform&, float, float), (const, override));
-	MOCK_METHOD(void, render_rigid_body, (const RigidBody&, Transform&), (const, override));
+	MOCK_METHOD(void, render_rigid_body, (const RigidBody&), (const, override));
 	MOCK_METHOD(Vector2d, transform_vector, (const Vector2d&), (const, override));
 	MOCK_METHOD(Vector2d, world_space_to_screen, (const Vector2d&), (const, override));
 	MOCK_METHOD(Vector2d, screen_space_to_screen, (const Vector2d&), (const, override));
 	MOCK_METHOD(Vector2d, screen_to_screen_space, (const Vector2d&), (const, override));
 	MOCK_METHOD(void, render_lines, (std::vector<Vector2d>&, const Color&), (const, override));
 	MOCK_METHOD(void, add_render_call, (RenderCall&), (override));
-	MOCK_METHOD(void, clear, (const Color&), (const, override));
+	MOCK_METHOD(void, clear, (), (const, override));
 	MOCK_METHOD(void, push_to_screen, (), (override));
 	MOCK_METHOD(void, toggle_fullscreen, (), (override));
 	MOCK_METHOD(void, toggle_debug_mode, (), (override));
