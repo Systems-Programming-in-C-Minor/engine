a1c58dae14e9d14424e0eaa0ca807d4e0f3bb9f9
marco_rivia <marco1088@hotmail.nl>
2022-12-19 21:06:33 +0100

    start adding ui object docs

--- include/uiobjects/text.hpp
+++ include/uiobjects/text.hpp
@@ -17,6 +17,21 @@ private:
     Color _color;
     int _order_in_layer;
 public:
+    /**
+     * @brief Constructor.
+     * @param name The name of the text GameObject.
+     * @param tag The tag of the text GameObject.
+     * @param is_world_space Boolean option if the text will be rendered in world space.
+     * @param transform The transform of the text.
+     * @param width The width of the text.
+     * @param height The height of the text.
+     * @param text The actual text which will be shown.
+     * @param font The font of the text.
+     * @param size The size of the text.
+     * @param alignment The alignment enum of the text.
+     * @param color The color of the text.
+     * @param order_in_layer The order in which the text will be rendered.
+     */
     Text(const std::string& name, const std::string& tag, bool is_world_space, Transform transform, int width, int height,
         std::string  text, std::string  font, int size, Alignment alignment, Color color, int order_in_layer);
     [[nodiscard]] std::string get_text() const;
