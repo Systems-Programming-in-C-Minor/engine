a93bca8e6ba79c1b713343fd383962ff4c709d1b
marco_rivia <marco1088@hotmail.nl>
2022-12-19 09:37:34 +0100

    fix tests

--- test/tests/components/audiosource_tests.cpp
+++ test/tests/components/audiosource_tests.cpp
@@ -4,19 +4,19 @@
 
 TEST(AudioSourceTest, HasNotBeenPlayed_Active)
 {
-	AudioSource audio("./assets/sample.wav");
+	AudioSource audio("./assets/sample.mp3");
 	EXPECT_EQ(audio.get_play_count(), 0);
 }
 
 TEST(AudioSourceTest, HasBeenPlayed_Active)
 {
-	const AudioSource audio("./assets/sample.wav", true);
+	const AudioSource audio("./assets/sample.mp3", true);
 	EXPECT_EQ(audio.get_play_count(), 1);
 }
 
 TEST(AudioSourceTest, HasBeenPlayed_NotActiveToActive)
 {
-	AudioSource audio("./assets/sample.wav", true);
+	AudioSource audio("./assets/sample.mp3", true);
 	audio.set_active(false);
 	audio.set_active(true);
 	EXPECT_EQ(audio.get_play_count(), 2);
@@ -24,7 +24,7 @@ TEST(AudioSourceTest, HasBeenPlayed_NotActiveToActive)
 
 TEST(AudioSourceTest, HasBeenPlayed)
 {
-	AudioSource audio("./assets/sample.wav");
+	AudioSource audio("./assets/sample.mp3");
 	audio.play();
 	EXPECT_EQ(audio.get_play_count(), 1);
 }
@@ -38,7 +38,7 @@ TEST(AudioSourceTest, HasNotBeenPlayed_NoFile)
 
 TEST(AudioSourceTest, HasNotBeenPlayed_WrongFileType)
 {
-	AudioSource audio("./assets/sample.mp3");
+	AudioSource audio("./assets/sample.wav");
 	audio.play();
 	EXPECT_EQ(audio.get_play_count(), 0);
 }
