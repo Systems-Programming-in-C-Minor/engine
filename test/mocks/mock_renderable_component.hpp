3ee3db2664801976b829029be3deb9e249c27854
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-19 19:16:44 +0100

    Fixed tests

--- test/mocks/mock_renderable_component.hpp
+++ test/mocks/mock_renderable_component.hpp
@@ -7,7 +7,9 @@
 class MockRenderableComponent : public Component, public IRenderable {
 public:
 	explicit MockRenderableComponent(int order_in_layer) : IRenderable(order_in_layer) {}
-    MOCK_METHOD(void, render, (bool), (override, const));
+	MOCK_METHOD(void, set_active, (bool), (override));
+	MOCK_METHOD(void, render, (), (override));
+	MOCK_METHOD(int, get_order_in_layer, (), (override));
 };
 
 #endif //ENGINE_MOCK_RENDERABLE_COMPONENT_HPP
--- test/tests/camera_tests.cpp
+++ test/tests/camera_tests.cpp
@@ -1,27 +1,10 @@
 #include <gtest/gtest.h>
 #include "camera.hpp"
 
-TEST(CameraTest, GetAndSetBackgroundColor){
-    Camera test_camera {1280, 720};
-    Color test_color = {0,0,0,0};
+TEST(CameraTest, GetAndSetBackgroundColor) {
+    Camera test_camera;
+    Color test_color = { 0,0,0,0 };
 
     test_camera.set_background_color(test_color);
     EXPECT_EQ(test_color, test_camera.get_background_color());
 }
-
-TEST(CameraTest, GetAndSetAspectWidth){
-    Camera test_camera {1280, 720};
-    int test_value = 9999;
-
-    test_camera.set_aspect_width(test_value);
-    EXPECT_EQ(9999, test_camera.get_aspect_width());
-}
-
-TEST(CameraTest, GetAndSetAspectHeight){
-    Camera test_camera {1280, 720};
-    int test_value = 9999;
-
-    test_camera.set_aspect_height(test_value);
-    EXPECT_EQ(9999, test_camera.get_aspect_height());
-}
-
