86a215df1f4313390c46e8146abb5d6128eb3bf0
marco_rivia <marco1088@hotmail.nl>
2022-12-20 12:22:28 +0100

    add race docs

--- include/race/objects/checkpoint.hpp
+++ include/race/objects/checkpoint.hpp
@@ -4,8 +4,18 @@
 #include <gameobject.hpp>
 #include "optional"
 
+/**
+ * @brief Class representing a Checkpoint object.
+ */
 class Checkpoint : public GameObject {
 public:
+    /**
+     * @brief Constructor.
+     * @param name The name of the checkpoint.
+     * @param tag The tag of the checkpoint.
+     * @param is_finish_line Boolean value whether or not the checkpoint is a finish line.
+     * @param previous_checkpoint Optionally the previous checkpoint linked to this one.
+     */
     Checkpoint(const std::string &name, const std::string &tag, bool is_finish_line,
                std::optional<std::shared_ptr<Checkpoint>> previous_checkpoint);
 
