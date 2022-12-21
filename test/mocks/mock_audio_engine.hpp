93bbcc6b1c61549e061e524d82f45a45c506aaef
Maik de Kruif <maik@maik.dev>
2022-12-21 16:02:52 +0100

    Add new play, resume and is_playing methods

--- test/mocks/mock_audio_engine.hpp
+++ test/mocks/mock_audio_engine.hpp
@@ -6,15 +6,12 @@
 
 class MockAudioEngine : public ISoundEngine {
 public:
-    MOCK_METHOD(void, play, (const std::string&, double volume, bool loop), (override))
+    MOCK_METHOD(int, play, (const std::string&, double volume, bool loop), (override));
 
-    int
-
-    ;
-    MOCK_METHOD(void, stop, (const std::string&), (override))int
-
-    ;
-    MOCK_METHOD(void, load_chunk, (const std::string&), ());
+    MOCK_METHOD(void, play, (int), (override));
+    MOCK_METHOD(void, resume, (int), (override));
+    MOCK_METHOD(void, stop, (int), (override));
+    MOCK_METHOD(bool, is_playing, (int), (override, const));
 };
 
 #endif //ENGINE_MOCK_AUDIO_ENGINE_HPP
