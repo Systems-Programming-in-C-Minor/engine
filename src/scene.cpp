860277fdcc6fbbe7dc957ffaad1ff84c70ef465c
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-22 03:40:38 +0100

    Add scene tick return statement

--- src/scene.cpp
+++ src/scene.cpp
@@ -4,15 +4,19 @@
 #include "scene.hpp"
 #include "handlers/collision_handler.hpp"
 #include "camera.hpp"
+#include "global.hpp"
 
 void Scene::tick() {
+    const auto scene = &Global::get_instance()->get_engine().get_active_scene();
     for (auto &game_object: gameobjects) {
+        if (scene != &Global::get_instance()->get_engine().get_active_scene())
+            return;
         game_object->tick();
     }
 }
 
 void Scene::tick_world() {
-    _world->Step(1.f/60.f, 6.0f, 2.0f);
+    _world->Step(1.f / 60.f, 6.0f, 2.0f);
 }
 
 void Scene::render() const {
@@ -22,33 +26,29 @@ void Scene::render() const {
 }
 
 EventManager &Scene::get_event_manager() const {
-    return *_event_manager; 
+    return *_event_manager;
 }
 
-std::shared_ptr<Camera> Scene::get_camera() const
-{
+std::shared_ptr<Camera> Scene::get_camera() const {
     return _camera;
 }
 
-std::string Scene::get_name() const
-{
+std::string Scene::get_name() const {
     return _name;
 }
 
-void Scene::set_camera(std::shared_ptr<Camera> camera)
-{
+void Scene::set_camera(std::shared_ptr<Camera> camera) {
     _camera = std::move(camera);
 }
 
 Scene::~Scene() = default;
 
 Scene::Scene(std::shared_ptr<Camera> camera, std::string name) :
-    _event_manager(std::make_unique<EventManager>()),
-    _world(std::make_unique<b2World>(b2Vec2(0.0f, 0.0f))),
-    _collision_handler(std::make_unique<CollisionHandler>()),
-    _camera(std::move(camera)),
-    _name(std::move(name))
-{
+        _event_manager(std::make_unique<EventManager>()),
+        _world(std::make_unique<b2World>(b2Vec2(0.0f, 0.0f))),
+        _collision_handler(std::make_unique<CollisionHandler>()),
+        _camera(std::move(camera)),
+        _name(std::move(name)) {
     _world->SetContactListener(_collision_handler.get());
     gameobjects.push_back(_camera);
 }
